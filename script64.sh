7d962f00b9ccee6b19fad68c18be060a98e5c85a
Ruizhou Ding <rzhding@gmail.com>
2018-04-28 17:48:22 -0400

    cifar10 more superclasses

--- script64.sh
+++ script64.sh
@@ -1,4 +1,4 @@
-python main_f2c.py --superclass 01389 --gpus 1 &&
-python main_f2c.py --superclass 02489 --gpus 1 &&
-python main_f2c.py --superclass 25789 --gpus 1 &&
-python main_f2c.py --superclass 12348 --gpus 1 
+python main_f2c.py --superclass 0129 --gpus 1 &&
+python main_f2c.py --superclass 1689 --gpus 1 &&
+python main_f2c.py --superclass 0135 --gpus 1 &&
+python main_f2c.py --superclass 0134 --gpus 1 
