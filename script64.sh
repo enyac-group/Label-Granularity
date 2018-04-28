be4e61ab4d809060993ce4b670ee2e5450335e65
Ruizhou Ding <rzhding@gmail.com>
2018-04-28 17:52:31 -0400

    cifar10 more superclasses

--- script64.sh
+++ script64.sh
@@ -1,4 +1,4 @@
-python main_f2c.py --superclass 0129 --gpus 1 &&
-python main_f2c.py --superclass 1689 --gpus 1 &&
-python main_f2c.py --superclass 0135 --gpus 1 &&
-python main_f2c.py --superclass 0134 --gpus 1 
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --superclass 0129 --gpus 1 &&
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --superclass 1689 --gpus 1 &&
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --superclass 0135 --gpus 1 &&
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --superclass 0134 --gpus 1 
