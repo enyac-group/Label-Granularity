34438ed396fc960bb78e137c4e4f523c12e611e4
Ruizhou Ding <rzhding@gmail.com>
2018-04-29 10:57:58 -0400

    cifar10 more superclasses

--- script55_t10t2.sh
+++ script55_t10t2.sh
@@ -1,4 +1,4 @@
-python main_f2c.py --superclass 01389 --gpus 0 --resume --resume_dir 2018-04-23_03-38-31 &&
-python main_f2c.py --superclass 02489 --gpus 0 --resume --resume_dir 2018-04-23_03-38-31 &&
-python main_f2c.py --superclass 25789 --gpus 0 --resume --resume_dir 2018-04-23_03-38-31 &&
-python main_f2c.py --superclass 12348 --gpus 0 --resume --resume_dir 2018-04-23_03-38-31 
+python main_f2c.py --superclass 01389 --gpus 0 --resume --resume_dir results/2018-04-23_03-38-31 &&
+python main_f2c.py --superclass 02489 --gpus 0 --resume --resume_dir results/2018-04-23_03-38-31 &&
+python main_f2c.py --superclass 25789 --gpus 0 --resume --resume_dir results/2018-04-23_03-38-31 &&
+python main_f2c.py --superclass 12348 --gpus 0 --resume --resume_dir results/2018-04-23_03-38-31 
