c7d2047c4ce6820b46426271d9604c86c179c69d
Zhuo <zhuochencmu@gmail.com>
2018-04-28 21:30:15 -0400

    fix a bug that it plots multiple means in per class density

--- visual_cluster_cifar10.py
+++ visual_cluster_cifar10.py
@@ -11,11 +11,11 @@ file_cifar100_20_feature = '2018-04-26_17-55-54'
 
 np.random.seed(1234)
 
-coarse_classes = ('0','1')
-
-fine_classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-classes_c2f = {'0': ['bird', 'plane', 'car', 'ship', 'truck'],
+#coarse_classes = ('0','1')
+#
+#fine_classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+#
+#classes_c2f = {'0': ['bird', 'plane', 'car', 'ship', 'truck'],
 		'1': ['cat', 'deer', 'dog', 'frog', 'horse']}
 
 
@@ -47,17 +47,17 @@ def compute_class_variance(labels, features, exp_name=''):
 
 print 'Reading file...'
 with open(os.path.join(results_dir, file_cifar100_100_feature, 'train_feats.pkl'), 'r') as f:
-	cifar100_100_feature = pickle.load(f)
+	fine_feat = pickle.load(f)
 with open(os.path.join(results_dir, file_cifar100_100_feature, 'debug.pkl'), 'r') as f:
-	cifar100_100_label = np.array(pickle.load(f)[1], dtype=np.float)
-#print cifar100_100_feature.shape
-print 'cifar100 GT label 10 fine class:', cifar100_100_label
+	fine_label = np.array(pickle.load(f)[1], dtype=np.float)
+#print fine_feat.shape
+print 'cifar100 GT label 10 fine class:', fine_label
 with open(os.path.join(results_dir, file_cifar100_20_feature, 'train_feats.pkl'), 'r') as f:
-	cifar100_20_feature = pickle.load(f)
+	coarse_feat = pickle.load(f)
 with open(os.path.join(results_dir, file_cifar100_20_feature, 'debug.pkl'), 'r') as f:
-	cifar100_20_label = np.array(pickle.load(f)[1],dtype=np.float)
-#print cifar100_20_feature.shape
-print 'cifar100 GT label 2 coarse class', cifar100_20_label
+	coarse_label = np.array(pickle.load(f)[1],dtype=np.float)
+#print coarse_feat.shape
+print 'cifar100 GT label 2 coarse class', coarse_label
 
 print 'Generating fine to coarse index map...'
 f2c_idx_map=[] # fine to coarse index map
@@ -67,32 +67,32 @@ for fine in fine_classes:
 			f2c_idx_map.append(coarse_classes.index(coarse_label))
 			break
 
-#cifar100_100on20_label = np.zeros(cifar100_100_label.shape)
-#for idx, label in enumerate(list(cifar100_100_label)):
+#cifar100_100on20_label = np.zeros(fine_label.shape)
+#for idx, label in enumerate(list(fine_label)):
 #	cifar100_100on20_label[idx] = (f2c_idx_map[int(label)])
 #
-print set(list(cifar100_100_label))
+print set(list(fine_label))
 #print set(list(cifar100_100on20_label))
-print set(list(cifar100_20_label))
+print set(list(coarse_label))
 
 
 sample_size = 20000
-total_size = cifar100_100_label.shape[0]
+total_size = fine_label.shape[0]
 shuffle_idx = np.random.permutation(total_size)[:sample_size]
 
-cifar100_100_fc7_rwn1 = normalize_r(cifar100_100_feature[shuffle_idx,:])
-cifar100_20_fc7_rwn1 = normalize_r(cifar100_20_feature[shuffle_idx,:])
+cifar100_100_fc7_rwn1 = normalize_r(fine_feat[shuffle_idx,:])
+cifar100_20_fc7_rwn1 = normalize_r(coarse_feat[shuffle_idx,:])
 
 ### compute inter and intra class variance across the entire training dataset
-coarse_mean 	= compute_class_variance(cifar100_20_label, normalize_r(cifar100_20_feature), exp_name='cifar10_2')
-f2c_mean 	= compute_class_variance(cifar100_20_label, normalize_r(cifar100_100_feature), exp_name='cifar10_10on2')
+coarse_mean 	= compute_class_variance(coarse_label, normalize_r(coarse_feat), exp_name='cifar10_2')
+f2c_mean 	= compute_class_variance(coarse_label, normalize_r(fine_feat), exp_name='cifar10_10on2')
 #print 'f2c_mean distance:', np.linalg.norm(f2c_mean[0,:]-f2c_mean[1,:], ord=2, axis=0)
-fine_mean 	= compute_class_variance(cifar100_100_label, normalize_r(cifar100_100_feature), exp_name='cifar10_10')
+fine_mean 	= compute_class_variance(fine_label, normalize_r(fine_feat), exp_name='cifar10_10')
 
-intra_class_dist_temp, mean_temp = sum_square_distance(normalize_r(cifar100_20_feature))
-print 'cifar10_2 Overall variance: ', intra_class_dist_temp/(cifar100_20_feature.shape[0])
-intra_class_dist_temp, mean_temp = sum_square_distance(normalize_r(cifar100_100_feature))
-print 'cifar10_10 Overall variance: ', intra_class_dist_temp/(cifar100_100_feature.shape[0])
+intra_class_dist_temp, mean_temp = sum_square_distance(normalize_r(coarse_feat))
+print 'cifar10_2 Overall variance: ', intra_class_dist_temp/(coarse_feat.shape[0])
+intra_class_dist_temp, mean_temp = sum_square_distance(normalize_r(fine_feat))
+print 'cifar10_10 Overall variance: ', intra_class_dist_temp/(fine_feat.shape[0])
 
 	
 use_PCA=0
@@ -106,9 +106,9 @@ else:
 	cifar100_20_fc7_rwn1_pca  = np.vstack((cifar100_20_fc7_rwn1,coarse_mean))
 
 print 'tSNE on CIFAR10_10...'
-cifar100_100_fc7_rwn1_embedded = TSNE().fit_transform(cifar100_100_fc7_rwn1_pca)
+fine_fc7_rwn1_tsne = TSNE().fit_transform(cifar100_100_fc7_rwn1_pca)
 print 'tSNE on CIFAR10_2...'
-cifar100_20_fc7_rwn1_embedded = TSNE().fit_transform(cifar100_20_fc7_rwn1_pca)
+coarse_fc7_rwn1_tsne = TSNE().fit_transform(cifar100_20_fc7_rwn1_pca)
 
 plot_data=1
 plot_density=1
@@ -128,26 +128,26 @@ def plot_density(x, y, mean=[], filename='noname.jpg'):
 
 if plot_data:
 	print 'Plotting projected data...'
-	plot_fig(cifar100_100_fc7_rwn1_embedded[:-12,0], cifar100_100_fc7_rwn1_embedded[:-12,1], mean=[cifar100_100_fc7_rwn1_embedded[-12:-2,0], cifar100_100_fc7_rwn1_embedded[-12:-2,1]], label=cifar100_100_label[shuffle_idx],filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
+	plot_fig(fine_fc7_rwn1_tsne[:-12,0], fine_fc7_rwn1_tsne[:-12,1], mean=[fine_fc7_rwn1_tsne[-12:-2,0], fine_fc7_rwn1_tsne[-12:-2,1]], label=fine_label[shuffle_idx],filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
 
-	plot_fig(cifar100_100_fc7_rwn1_embedded[:-12,0], cifar100_100_fc7_rwn1_embedded[:-12,1], label=cifar100_20_label[shuffle_idx], mean=[cifar100_100_fc7_rwn1_embedded[-2:,0], cifar100_100_fc7_rwn1_embedded[-2:,1]],  filename='cifar10_10on2_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
+	plot_fig(fine_fc7_rwn1_tsne[:-12,0], fine_fc7_rwn1_tsne[:-12,1], label=coarse_label[shuffle_idx], mean=[fine_fc7_rwn1_tsne[-2:,0], fine_fc7_rwn1_tsne[-2:,1]],  filename='cifar10_10on2_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
 
-	plot_fig(cifar100_20_fc7_rwn1_embedded[:-2,0], cifar100_20_fc7_rwn1_embedded[:-2,1], label=cifar100_20_label[shuffle_idx], mean=[cifar100_20_fc7_rwn1_embedded[-2:,0], cifar100_20_fc7_rwn1_embedded[-2:,1]], filename='cifar10_2_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
+	plot_fig(coarse_fc7_rwn1_tsne[:-2,0], coarse_fc7_rwn1_tsne[:-2,1], label=coarse_label[shuffle_idx], mean=[coarse_fc7_rwn1_tsne[-2:,0], coarse_fc7_rwn1_tsne[-2:,1]], filename='cifar10_2_fc7_rwn1_TSNE_sample-{0}.jpg'.format(sample_size))
 
 if plot_density:
 	print 'Plotting density...'
-	temp_label = cifar100_100_label[shuffle_idx]
+	temp_label = fine_label[shuffle_idx]
 	for i in range(10):
 		temp_idx = (temp_label == i)
-		plot_density(cifar100_100_fc7_rwn1_embedded[temp_idx,0], cifar100_100_fc7_rwn1_embedded[temp_idx,1], mean=[cifar100_100_fc7_rwn1_embedded[-12+i,0], cifar100_100_fc7_rwn1_embedded[-12+i,1]], filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
-	#print cifar100_100_fc7_rwn1_embedded[-2:,0], cifar100_100_fc7_rwn1_embedded[-2:,1]
+		plot_density(fine_fc7_rwn1_tsne[temp_idx,0], fine_fc7_rwn1_tsne[temp_idx,1], mean=[fine_fc7_rwn1_tsne[-12+i,0], fine_fc7_rwn1_tsne[-12+i,1]], filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
+	#print fine_fc7_rwn1_tsne[-2:,0], fine_fc7_rwn1_tsne[-2:,1]
 
-	plot_density(cifar100_100_fc7_rwn1_embedded[:-12,0], cifar100_100_fc7_rwn1_embedded[:-12,1], mean=[cifar100_100_fc7_rwn1_embedded[-12:-2,0], cifar100_100_fc7_rwn1_embedded[-12:-2,1]], filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}_density_allclass.jpg'.format(sample_size))
+	plot_density(fine_fc7_rwn1_tsne[:-12,0], fine_fc7_rwn1_tsne[:-12,1], mean=[fine_fc7_rwn1_tsne[-12:-2,0], fine_fc7_rwn1_tsne[-12:-2,1]], filename='cifar10_10_fc7_rwn1_TSNE_sample-{0}_density_allclass.jpg'.format(sample_size))
 	
-	temp_label = cifar100_20_label[shuffle_idx]
+	temp_label = coarse_label[shuffle_idx]
 	for i in range(2):
 		temp_idx = (temp_label == i)
-		plot_density(cifar100_100_fc7_rwn1_embedded[temp_idx,0], cifar100_100_fc7_rwn1_embedded[temp_idx,1], mean=[cifar100_100_fc7_rwn1_embedded[-2+i:,0], cifar100_100_fc7_rwn1_embedded[-2+i:,1]], filename='cifar10_10on2_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
+		plot_density(fine_fc7_rwn1_tsne[temp_idx,0], fine_fc7_rwn1_tsne[temp_idx,1], mean=[fine_fc7_rwn1_tsne[-2+i,0], fine_fc7_rwn1_tsne[-2+i,1]], filename='cifar10_10on2_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
 
-		plot_density(cifar100_20_fc7_rwn1_embedded[temp_idx,0], cifar100_20_fc7_rwn1_embedded[temp_idx,1], mean=[cifar100_20_fc7_rwn1_embedded[-2+i:,0], cifar100_20_fc7_rwn1_embedded[-2+i:,1]], filename='cifar10_2_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
+		plot_density(coarse_fc7_rwn1_tsne[temp_idx,0], coarse_fc7_rwn1_tsne[temp_idx,1], mean=[coarse_fc7_rwn1_tsne[-2+i,0], coarse_fc7_rwn1_tsne[-2+i,1]], filename='cifar10_2_fc7_rwn1_TSNE_sample-{0}_density_class{1}.jpg'.format(sample_size,i))
 
