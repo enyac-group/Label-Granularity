322fbf4c2fd45b45d1bf832b1e8b2e9204a89856
Ruizhou Ding <rzhding@gmail.com>
2018-05-03 23:49:45 -0400

    f_10_20

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -74,7 +74,7 @@ class PreActResNet(nn.Module):
         self.layer2 = self._make_layer(block, thickness*2, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, thickness*4, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, thickness*8, num_blocks[3], stride=2)
-        self.linear = nn.Linear(thickness*4*block.expansion, num_classes)
+        self.linear = nn.Linear(thickness*8*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
