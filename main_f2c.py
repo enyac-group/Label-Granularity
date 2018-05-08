7c08f4dc0444ea2e1870fa49aad8c2b74997298b
Ruizhou Ding <rzhding@gmail.com>
2018-05-08 14:43:05 -0400

    fine tune

--- main_f2c.py
+++ main_f2c.py
@@ -121,7 +121,7 @@ if args.resume:
     checkpoint = torch.load(os.path.join(args.resume_dir, 'ckpt.t7'))
     net_dict = net.state_dict()
     net_dict.update(checkpoint['net'].state_dict())
-    net.load_state_dict(net_dict)
+    net.load_state_dict(net_dict, strict=False)
 
 
 logging.info("model structure: %s", net)
