fee8df7e9c810c047e8d1d63754d1bb1a0ca120d
Ruizhou Ding <rzhding@gmail.com>
2018-05-10 15:58:03 -0400

    imagenet

--- main_f2c_imagenet.py
+++ main_f2c_imagenet.py
@@ -69,7 +69,7 @@ for idx,a_class in enumerate(classes):
         classes_f2c[idx] = 1
 
 transform_train = transforms.Compose([
-    transforms.RandomCrop(224),
+    transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
