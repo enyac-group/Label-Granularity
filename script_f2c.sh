78c0023c31120f826d2d4e923a6203d8a3eb7839
Ruizhou Ding <rzhding@gmail.com>
2018-05-14 00:02:19 -0400

    shuffle for fewer data

--- script_f2c.sh
+++ script_f2c.sh
@@ -7,14 +7,4 @@ python main_f2c_imagenet.py --f2c 1 --categories dog_cat --data_ratio 0.6 &&
 python main_f2c_imagenet.py --f2c 0 --categories dog_cat --data_ratio 0.8 &&
 python main_f2c_imagenet.py --f2c 1 --categories dog_cat --data_ratio 0.8 &&
 python main_f2c_imagenet.py --f2c 0 --categories dog_cat --data_ratio 1. &&
-python main_f2c_imagenet.py --f2c 1 --categories dog_cat --data_ratio 1. &&
-python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 0.2 &&
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 0.2 &&
-python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 0.4 &&
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 0.4 &&
-python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 0.6 &&
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 0.6 &&
-python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 0.8 &&
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 0.8 &&
-python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 1. &&
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 1. 
+python main_f2c_imagenet.py --f2c 1 --categories dog_cat --data_ratio 1. 
