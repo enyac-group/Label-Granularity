58dee72ce18a5e29cb0d5ff6419c6a757f854415
Ruizhou Ding <rzhding@gmail.com>
2018-05-15 09:50:37 -0400

    only dropout last layer

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -74,7 +74,7 @@ class PreActResNet(nn.Module):
         self.layer2 = self._make_layer(block, thickness*2, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, thickness*4, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, thickness*8, num_blocks[3], stride=2)
-        self.dropout = nn.Dropout(thickness*8*block.expansion, p=dropout)
+        self.dropout = nn.Dropout(p=dropout)
         self.linear = nn.Linear(thickness*8*block.expansion, num_classes)
 
 
