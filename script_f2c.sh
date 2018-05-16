a3b2dd942b6d282a8e6aa6661b910799ebe46df2
Ruizhou Ding <rzhding@gmail.com>
2018-05-16 13:33:38 -0400

    only dropout last layer

--- script_f2c.sh
+++ script_f2c.sh
@@ -1,2 +1,2 @@
-python main_f2c_imagenet.py --f2c 1 --categories fruit_vege --data_ratio 1. &&
-python main_f2c_imagenet.py --f2c 1 --categories dog_cat --data_ratio 1.
+python main_f2c_imagenet.py --f2c 0 --categories fruit_vege --data_ratio 1. &&
+python main_f2c_imagenet.py --f2c 0 --categories dog_cat --data_ratio 1.
