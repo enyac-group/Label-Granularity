a4ef922875b2d1f1616bed96d508bbb04c2eb9a9
Ruizhou Ding <rzhding@gmail.com>
2018-05-23 09:06:21 -0400

    cifar10 data ratio

--- script_f2c_cifar10.sh
+++ script_f2c_cifar10.sh
@@ -1,10 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.2 --categories animals &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.2 --categories animals &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.2 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.2 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c.py --f2c 0 --data_ratio 0.4 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c.py --f2c 1 --data_ratio 0.4 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.4 --categories animals &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.4 --categories animals &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.4 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.4 
+CUDA_VISIBLE_DEVICES=0 python main_f2c.py --f2c 0 --data_ratio 0.4 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c.py --f2c 1 --data_ratio 0.4 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.4 --categories animals &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.4 --categories animals &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.2 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c_cifar100.py --f2c 0 --data_ratio 0.4 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c_cifar100.py --f2c 1 --data_ratio 0.4 
