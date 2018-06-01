c0efe1d9fc37ec93321d64e21e127f299e8bacf9
Ruizhou Ding <rzhding@gmail.com>
2018-06-01 15:01:02 -0700

    change super class number

--- scripts/script_f2c_superclasses_gpu1_rd.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --categories 5_classes --f2c 1 --data_ratio 1. --add_layer 0 &&
-CUDA_VISIBLE_DEVICES=1 python main_f2c_cifar100.py --categories 15_classes --f2c 1 --data_ratio 1. --add_layer 0 
