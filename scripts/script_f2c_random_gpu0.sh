f6a81c2d7fe1a973f4757e174b6e5577d7993bb2
Ruizhou Ding <rzhding@gmail.com>
2018-06-02 23:00:33 -0700

    generate random fine-class data for cifar10

---
+++ scripts/script_f2c_random_gpu0.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python main_f2c.py --f2c 0 --randomness 0.001 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c.py --f2c 0 --randomness 0.01 &&
+CUDA_VISIBLE_DEVICES=0 python main_f2c.py --f2c 0 --randomness 0.1 
---
+++ scripts/script_f2c_random_gpu1.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --f2c 0 --randomness 0.003 &&
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --f2c 0 --randomness 0.03 &&
+CUDA_VISIBLE_DEVICES=1 python main_f2c.py --f2c 0 --randomness 0.3 
