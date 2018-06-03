f2edaf6d87153ff595f49b6f780fbb5d15c1e9b2
Ruizhou Ding <rzhding@gmail.com>
2018-06-02 18:23:24 -0700

    gen confmat

--- utils_confmat.py
+++ utils_confmat.py
@@ -68,6 +68,7 @@ def confusion(net, loader, classes_f2c):
     print('normalized confusion matrix: \n{}'.format(conf_matrix_nrm))
     num_coarse_classes = max([c for f,c in enumerate(classes_f2c)])
     group = [[f for f,c in enumerate(classes_f2c) if c == c_cls] for c_cls in range(num_coarse_classes)]
+    print('group: {}'.format(group))
     inter_confusion = inter_conf(conf_matrix_nrm, group=group)
     intra_confusion = intra_conf(conf_matrix_nrm, group=group)
     return inter_confusion, intra_confusion
